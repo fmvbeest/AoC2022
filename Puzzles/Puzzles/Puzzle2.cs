5e778eb9e8d652c4c3b9b0db83132f53d59da83e
martijn <fmvbeest@gmail.com>
2022-12-04 10:55:40 +0100

    Refactor puzzle 2

--- Puzzles/Puzzles/Puzzle2.cs
+++ Puzzles/Puzzles/Puzzle2.cs
@@ -4,99 +4,105 @@ public class Puzzle2 : PuzzleBase
 {
     protected override string Filename => "Input/puzzle-input-02";
     protected override string PuzzleTitle => "--- Day 2: Rock Paper Scissors ---";
-
+    
     public override int PartOne(IPuzzleInput input)
     {
-        var total = 0;
-        
-        foreach (var line in input.GetAllLines())
-        {
-            var x = ShapeMap[line[0]];
-            var y = ShapeMap[line[2]];
-            var r = Result[(y, x)];
-
-            total += GetPoints(r, y);
-        }
-
-        return total;
+        return Preprocess(input).Sum(RPS.GetPoints);
     }
 
     public override int PartTwo(IPuzzleInput input)
     {
-        var total = 0;
-        
-        foreach (var line in input.GetAllLines())
+        return Preprocess(input).Sum(game => RPS.GetPoints(RPS.FixGame(game)));
+    }
+
+    private IEnumerable<RPSInstance> Preprocess(IPuzzleInput input)
+    {
+        return input.GetAllLines().Select(line => new RPSInstance()
         {
-            var x = ShapeMap[line[0]];
-            var y = ExpectedResult[line[2]];
-            var m = SuggestedMoves[(x, y)];
-            var r = Result[(m, x)];
+            TheirMove = ParseMove(line[0]), 
+            OurMove = ParseMove(line[2]), 
+            ExpectedOutcome = ParseOutcome(line[2])
+        }).ToList();
+    }
 
-            total += GetPoints(r, m);
-        }
+    private static RPS.Move ParseMove(char move) => move switch
+    {
+        'A' => RPS.Move.Rock,
+        'B' => RPS.Move.Paper,
+        'C' => RPS.Move.Scissor,
+        'X' => RPS.Move.Rock,
+        'Y' => RPS.Move.Paper,
+        'Z' => RPS.Move.Scissor,
+        _ => throw new ArgumentOutOfRangeException(nameof(move), move, "Unexpected move")
+    };
         
-        return total;
+    private static RPS.Result ParseOutcome(char move) => move switch
+    {
+        'X' => RPS.Result.Loss,
+        'Y' => RPS.Result.Draw,
+        'Z' => RPS.Result.Win,
+        _ => throw new ArgumentOutOfRangeException(nameof(move), move, "Unexpected outcome")
+    };
+
+    private class RPSInstance
+    {
+        public RPS.Move TheirMove { get; init; }
+        public RPS.Move OurMove { get; set; }
+        public RPS.Result ExpectedOutcome { get; init; }
     }
-
-    private static int GetPoints(char result, char shape)
+    
+    private static class RPS
     {
-        return PointsResult[result] + PointsShape[shape];
+        public enum Move
+        {
+            Rock = 1, Paper = 2, Scissor = 3
+        }
+
+        public enum Result
+        {
+            Loss = 0, Draw = 3, Win = 6
+        }
+
+        public static int GetPoints(RPSInstance game)
+        {
+            return (int)GetResult(game) + (int)game.OurMove;
+        }
+
+        public static RPSInstance FixGame(RPSInstance game)
+        {
+            game.OurMove = SuggestedMoves[(game.TheirMove, game.ExpectedOutcome)];
+            return game;
+        }
+
+        private static Result GetResult(RPSInstance game)
+        {
+            return Outcome[(game.OurMove, game.TheirMove)];
+        }
+
+        private static readonly Dictionary<(Move, Move), Result> Outcome = new()
+        {
+            { (Move.Rock, Move.Scissor), Result.Win },
+            { (Move.Scissor, Move.Paper), Result.Win },
+            { (Move.Paper, Move.Rock), Result.Win },
+            { (Move.Scissor, Move.Rock), Result.Loss },
+            { (Move.Paper, Move.Scissor), Result.Loss },
+            { (Move.Rock, Move.Paper), Result.Loss },
+            { (Move.Rock, Move.Rock), Result.Draw },
+            { (Move.Paper, Move.Paper), Result.Draw },
+            { (Move.Scissor, Move.Scissor), Result.Draw }
+        };
+
+        private static readonly Dictionary<(Move, Result), Move> SuggestedMoves = new()
+        {
+            { (Move.Rock, Result.Win), Move.Paper },
+            { (Move.Rock, Result.Loss), Move.Scissor },
+            { (Move.Rock, Result.Draw), Move.Rock },
+            { (Move.Scissor, Result.Win), Move.Rock },
+            { (Move.Scissor, Result.Loss), Move.Paper },
+            { (Move.Scissor, Result.Draw), Move.Scissor },
+            { (Move.Paper, Result.Win), Move.Scissor },
+            { (Move.Paper, Result.Loss), Move.Rock },
+            { (Move.Paper, Result.Draw), Move.Paper }
+        };
     }
-
-    private static readonly Dictionary<char, int> PointsResult = new()
-    {
-        { 'L', 0 },
-        { 'D', 3 },
-        { 'W', 6 }
-    };
-    
-    private static readonly Dictionary<char, int> PointsShape = new()
-    {
-        { 'R', 1 },
-        { 'P', 2 },
-        { 'S', 3 }
-    };
-
-    private static readonly Dictionary<char, char> ShapeMap = new()
-    {
-        { 'A', 'R' },
-        { 'B', 'P' },
-        { 'C', 'S' },
-        { 'X', 'R' },
-        { 'Y', 'P' },
-        { 'Z', 'S' }
-    };
-
-    private static readonly Dictionary<(char, char), char> Result = new()
-    {
-        { ('R', 'S'), 'W' },
-        { ('S', 'P'), 'W' },
-        { ('P', 'R'), 'W' },
-        { ('S', 'R'), 'L' },
-        { ('P', 'S'), 'L' },
-        { ('R', 'P'), 'L' },
-        { ('R', 'R'), 'D' },
-        { ('P', 'P'), 'D' },
-        { ('S', 'S'), 'D' }
-    };
-    
-    private static readonly Dictionary<char, char> ExpectedResult = new()
-    {
-        { 'X', 'L' },
-        { 'Y', 'D' },
-        { 'Z', 'W' }
-    };
-    
-    private static readonly Dictionary<(char, char), char> SuggestedMoves = new()
-    {
-        { ('R', 'W'), 'P' },
-        { ('R', 'L'), 'S' },
-        { ('R', 'D'), 'R' },
-        { ('S', 'W'), 'R' },
-        { ('S', 'L'), 'P' },
-        { ('S', 'D'), 'S' },
-        { ('P', 'W'), 'S' },
-        { ('P', 'L'), 'R' },
-        { ('P', 'D'), 'P' }
-    };
 }
